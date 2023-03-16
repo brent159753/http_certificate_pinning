3817d94f6afa8388474e51c3906ce9132c5eb12b
Brent Boderé <brent.bodere@rydoo.com>
2023-03-16 10:00:51 +0100

    Revert "callFollowingErrorInterceptor = true"
    This reverts commit fd8b73bd57989c3f12eb97de78626a3fb6882e8a.

--- lib/src/dio/certificate_pinning_interceptor.dart
+++ lib/src/dio/certificate_pinning_interceptor.dart
@@ -56,7 +56,6 @@ class CertificatePinningInterceptor extends Interceptor {
             requestOptions: options,
             error: CertificateNotVerifiedException(),
           ),
-          true,
         );
       }
     } on Exception catch (e) {
@@ -73,7 +72,6 @@ class CertificatePinningInterceptor extends Interceptor {
           requestOptions: options,
           error: error,
         ),
-        true,
       );
     }
   }
